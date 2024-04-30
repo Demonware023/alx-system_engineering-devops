11dba6cef92be78f1ca584d60f43950337905242
Demonware023 <awamchimere@icloud.com>
2024-04-29 21:53:57 -0700

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,70 +1,28 @@
-# Updating Packages before performing installations
-exec { 'apt_update':
-  command => '/usr/bin/apt-get update',
-  path    => '/usr/bin',
-  require => Package['nginx'],
-}
+# Automating project requirements using Puppet
 
-# Installing Nginx
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt_update'],
+  ensure => installed,
 }
 
-# Creating an index.html page
+file_line { 'redirect_rule':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
+}
+
+file_line { 'custom_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;',
+}
+
 file { '/var/www/html/index.html':
   content => 'Hello World!',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  require => Package['nginx'],
 }
 
-# Performing a "moved permanently redirection" (301)
-$rewrite_rule = 'server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/besthor permanent;'
-file_line { 'nginx_redirect_rule':
-  path    => '/etc/nginx/sites-enabled/default',
-  line    => $rewrite_rule,
-  require => Package['nginx'],
-}
-
-# Creating a 404 Custom error page
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page",
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  require => Package['nginx'],
-}
-
-# Creating an HTTP response header
-$hostname = $facts['hostname']
-$custom_header = "add_header X-Served-By $hostname;"
-file_line { 'nginx_custom_header':
-  path    => '/etc/nginx/sites-enabled/default',
-  line    => $custom_header,
-  require => Package['nginx'],
-}
-
-# Testing configurations for syntax errors
-exec { 'nginx_syntax_check':
-  command => '/usr/sbin/nginx -t',
-  path    => '/usr/sbin',
-  require => Package['nginx'],
-}
-
-# Restarting Nginx after implementing changes
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => [
-    File_line['nginx_redirect_rule'],
-    File_line['nginx_custom_header'],
-    Exec['nginx_syntax_check'],
-  ],
-  require   => [
-    Package['nginx'],
-    File['/var/www/html/index.html'],
-    File['/var/www/html/404.html'],
-  ],
+  ensure  => running,
+  require => Package['nginx'],
 }
