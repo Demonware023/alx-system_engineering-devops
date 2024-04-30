ed9b7d988490808ccfa4c26ca802fccf07a59486
Demonware023 <awamchimere@icloud.com>
2024-04-29 21:55:40 -0700

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,25 +4,49 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'redirect_rule':
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => 'Ceci n\'est pas une page',
+  require => Package['nginx'],
+}
+
+file_line { 'custom_404_error_page':
   ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
+  path   => '/etc/nginx/sites-available/default',
+  match  => '^error_page 404',
+  line   => 'error_page 404 /404.html;',
+}
+
+file_line { 'custom_404_error_page_location':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  match  => 'location = /404.html',
+  line   => "location = /404.html {\n\troot /var/www/html;\n\tinternal;\n}",
+}
+
+file_line { 'redirect_permanent':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  match  => 'rewrite ^/redirect_me',
   line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
 }
 
-file_line { 'custom_header':
+file_line { 'custom_http_header':
   ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+  path   => '/etc/nginx/sites-available/default',
+  match  => 'add_header X-Served-By',
+  line   => "add_header X-Served-By \$hostname;",
 }
 
 service { 'nginx':
   ensure  => running,
+  enable  => true,
   require => Package['nginx'],
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
