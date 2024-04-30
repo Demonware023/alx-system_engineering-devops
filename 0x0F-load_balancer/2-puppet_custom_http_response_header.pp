f102388c89c895b61b419151bd6223e8fcbb9657
Demonware023 <awamchimere@icloud.com>
2024-04-29 22:49:13 -0700

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,10 +6,15 @@ package { 'nginx':
   ensure => installed,
 }
 
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
+}
+
 file { '/var/www/html/index.html':
-  ensure  => file,
   content => 'Hello World!',
-  require => Package['nginx'],
 }
 
 file_line { 'custom_http_header':
@@ -21,10 +26,5 @@ file_line { 'custom_http_header':
 
 service { 'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx'],
-  subscribe => File['/etc/nginx/sites-available/default'],
 }
-
-# Include the nginx_custom_header class to apply the configuration
-include nginx_custom_header
