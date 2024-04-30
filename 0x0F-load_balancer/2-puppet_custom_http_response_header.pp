ebd13859f65f04b1d155b7543ef9ea649e216b8b
Demonware023 <awamchimere@icloud.com>
2024-04-29 22:35:26 -0700

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,33 +10,6 @@ file { '/var/www/html/index.html':
   require => Package['nginx'],
 }
 
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => 'Ceci n\'est pas une page',
-  require => Package['nginx'],
-}
-
-file_line { 'custom_404_error_page':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  match  => '^error_page 404',
-  line   => 'error_page 404 /404.html;',
-}
-
-file_line { 'custom_404_error_page_location':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  match  => 'location = /404.html',
-  line   => "location = /404.html {\n\troot /var/www/html;\n\tinternal;\n}",
-}
-
-file_line { 'redirect_permanent':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  match  => 'rewrite ^/redirect_me',
-  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
-}
-
 file_line { 'custom_http_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
