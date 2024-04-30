d9c2a25e43802b6911345fbaf781830817792326
Demonware023 <awamchimere@icloud.com>
2024-04-29 22:41:33 -0700

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,24 @@
-# Automating project requirements using Puppet
+# Puppet manifest to configure Nginx with a custom HTTP header 'X-Served-By'
+class nginx_custom_header {
+  # Ensure Nginx is installed
+  package { 'nginx':
+    ensure => installed,
+  }
 
-package { 'nginx':
-  ensure => installed,
+  # Define a file resource for the Nginx default site configuration
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    require => Package['nginx'],
+    content => template('nginx/default.erb'),
+  }
+
+  # Service resource to manage the Nginx service
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
 }
 
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-file_line { 'custom_http_header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  match  => 'add_header X-Served-By',
-  line   => "add_header X-Served-By \$hostname;",
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
+# Include the nginx_custom_header class to apply the configuration
+include nginx_custom_header
