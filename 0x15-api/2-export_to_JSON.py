a5808d32e8aff0f78500186e356c9a604b9e6d5e
Demonware023 <awamchimere@icloud.com>
2024-05-20 13:46:39 -0700

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     user_url = f"{url}/users/{employee_id}"
     todos_url = f"{url}/todos?userId={employee_id}"
 
-    user_response = requests.get(user.url)
+    user_response = requests.get(user_url)
     todos_response = requests.get(todos_url)
 
     if user_response.status_code != 200:
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     user_data = user_response.json()
-    todos_data = todos_reponse.json()
+    todos_data = todos_response.json()
 
     user_id = str(user_data.get("id"))
     username = user_data.get("username")
