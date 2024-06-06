4767e0fc3b6ffd115fa96f61716913327b20e16b
Demonware023 <awamchimere@icloud.com>
2024-06-06 02:12:56 -0700

    Sign up for Datadog and install datadog-agent

--- 0x18-webstack_monitoring/check_datadog_host_exists.py
+++ 0x18-webstack_monitoring/check_datadog_host_exists.py
@@ -4,7 +4,7 @@ Get all hosts for your organization returns "OK" response
 """
 
 # Install the library and its dependencies 
-pip3 install datadog-api-client
+# pip3 install datadog-api-client
 
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v1.api.hosts_api import HostsApi
