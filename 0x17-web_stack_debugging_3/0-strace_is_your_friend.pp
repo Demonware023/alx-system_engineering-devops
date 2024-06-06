55d8cf7ad9f24904543e452501b752088d9013e7
Demonware023 <awamchimere@icloud.com>
2024-06-06 05:56:26 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,10 @@
-file { '/var/www/html/wordpress':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  command => '/path/to/your/fix/script.sh',
-  path    => ['/usr/bin', '/usr/sbin'],
-  mode    => '0755',
-  recurse => true,
+# A puppet manuscript to replace a line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
