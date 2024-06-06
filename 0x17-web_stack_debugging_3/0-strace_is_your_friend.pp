57c4f208ff4c7af4d9fac8c046b85d224bbc8a0e
Demonware023 <awamchimere@icloud.com>
2024-06-06 05:55:14 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,12 +2,8 @@ file { '/var/www/html/wordpress':
   ensure  => directory,
   owner   => 'www-data',
   group   => 'www-data',
+  command => '/path/to/your/fix/script.sh',
+  path    => ['/usr/bin', '/usr/sbin'],
   mode    => '0755',
   recurse => true,
 }
-
-exec { 'fix-apache':
-  command => '/path/to/your/fix/script.sh',
-  path    => ['/usr/bin', '/usr/sbin'],
-}
-
